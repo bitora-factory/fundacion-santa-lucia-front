464e768c6fbbcc0f4f31b853c294ea124b6edf9f
kevin-mauricio <kmma407@gmail.com>
2025-08-08 16:55:41 -0500

    feat: Update receipt component and styles for improved layout and functionality

--- src/app/components/receipt/receipt.component.ts
+++ src/app/components/receipt/receipt.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { BuildingComponent } from "../building/building.component";
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { CreateReceiptComponent } from './create/create-receipt.component';
+import { PaymentService } from '../../services/payment.service';
+import { takeUntil } from 'rxjs';
+import { AbstractComponent } from '../../abstract-component';
 
 @Component({
   selector: 'app-receipt',
@@ -17,22 +20,35 @@ import { CreateReceiptComponent } from './create/create-receipt.component';
   templateUrl: './receipt.component.html',
   styleUrl: './receipt.component.scss'
 })
-export class ReceiptComponent {
+export class ReceiptComponent extends AbstractComponent implements OnInit {
 
   payments: any[] = []; // Replace with actual type if available
+  private paymentService = inject(PaymentService)
 
 
   constructor() {
-    // Initialize or fetch payments data here
-  }
-/* 
-  getRegularColumns(): TableInterface[] {
-    return this.columns.filter(col => !col.frozen);
+    super();
   }
 
-  getFrozenColumns(): TableInterface[] {
-    return this.columns.filter(col => col.frozen);
-  } */
+  ngOnInit(): void {
+    this.loadPayments();
+  }
+
+  private loadPayments() {
+    this.paymentService.findAll()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(payments => {
+        this.payments = payments
+      });
+  }
+  /* 
+    getRegularColumns(): TableInterface[] {
+      return this.columns.filter(col => !col.frozen);
+    }
+  
+    getFrozenColumns(): TableInterface[] {
+      return this.columns.filter(col => col.frozen);
+    } */
 
 
 }
